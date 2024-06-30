17f62ff23df8282e014b7bacb57e07fc8bbc42de
박혜원 <s2028@e-mirim.hs.kr>
2024-06-30 23:52:46 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
@@ -1,9 +1,4 @@
 function solution(array) {
-    let answer = [array[0], 0];
-    for(let i = 1; i < array.length; i++) {
-        if(answer[0] < array[i]) {
-            answer = [array[i], i];
-        }
-    }
+    var answer = [];
     return answer;
 }
