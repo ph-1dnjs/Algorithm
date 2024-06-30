cf79c2ad22ab263f09557acc96b6f93f31114819
박혜원 <s2028@e-mirim.hs.kr>
2024-06-30 23:58:59 +0900

    [level 0] Title: 수 조작하기 1, Time: 2.00 ms, Memory: 35.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
@@ -0,0 +1,15 @@
+function solution(n, control) {
+    control.split('').forEach(c => {
+        switch(c) {
+            case 'w': n++;
+                break;
+            case 's': n--;
+                break;
+            case 'd': n+=10;
+                break;
+            case 'a': n-=10;
+                break;
+        }
+    })
+    return n;
+}
