2c29e4d1ae0fc150af7833107792b847131519b6
박혜원 <s2028@e-mirim.hs.kr>
2024-07-01 00:06:41 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.js
@@ -0,0 +1,14 @@
+function solution(num_list) {
+    let odd = '';
+    let even = '';
+    
+    num_list.forEach(num => {
+        if(num % 2 === 0) {
+            even += num;
+        } else {
+            odd += num;
+        }
+    })
+    
+    return Number(odd) + Number(even);
+}
